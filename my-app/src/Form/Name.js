860d83c06da6c16eb2fb8a06186993bc1ee70ec9
sangeetha2521 <sangeens8101@gmail.com>
2022-12-31 18:40:05 +0530

    Validation added

--- my-app/src/Form/Name.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Name({ register }) {
-  return (
-    <div className="Name">
-      <input {...register("firstName", { required: true })} placeholder="Enter your name"></input>
-    </div>
-  );
-}
--- my-app/src/Form/index.js
+++ my-app/src/Form/index.js
@@ -1,31 +1,51 @@
 import * as React from "react";
 import { useForm } from "react-hook-form";
-import Name from "./Name";
-import Password from "./Password";
 
 export default function Index() {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors },
-  } = useForm({
-    defaultValues: {
-      firstName: "",
-      password: "",
-    },
-  });
+    clearErrors,
+  } = useForm();
+  const [userInfo, setUserInfo] = React.useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    alert("Successfully submitted");
+  };
   return (
-    <form onSubmit={handleSubmit(console.log)}>
-      <div className="form-group">
-        <div className="React-form">
-          <h2>React form learning</h2>
-          <Name register={register} />
-          <Password register={register} errors={errors} setError={setError} />
-
-          <button>Submit</button>
+    <>
+      <p> {JSON.stringify(userInfo, undefined, 2)}</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <div className="React-form">
+            <h2>React Hook Form Learning</h2>
+            <input
+              name="username"
+              autoComplete="off"
+              placeholder="Enter username"
+              {...register("username", {
+                required: "User name required",
+              })}
+            />
+            <p>{errors.username && errors.username?.message}</p>
+            <input
+              name="password"
+              autoComplete="off"
+              placeholder="Enter password"
+              {...register("password", {
+                required: "password required",
+                maxLength: { value: 6, message: "Maximum 6 charectors are allowed" },
+              })}
+            />
+            <p>{errors.password && errors.password?.message}</p>
+            <button>Submit</button>
+            <button type="button" onClick={() => clearErrors()}>
+              Clear All Errors
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
